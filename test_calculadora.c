f341ee5bf2ea9bd284ceb5efc1df3fb276daa487
Luís Guilherme <luis.bl.guilherme@gmail.com>
2026-05-27 20:40:55 -0300

    test_calculadora.c

---
+++ test_calculadora.c
@@ -0,0 +1,15 @@
+//Desenvolvido por Luís Guilherme, 1° DS
+
+#include <assert.h>
+#include <stdio.h>
+#include "function_dividir.c"
+
+int main()
+{
+    assert(dividir(14,2) == 7);
+    assert(dividir(99,3) == 33);
+    assert(dividir(144,12) == 12);
+    printf("Teste executado com sucesso");
+
+    return 0;
+}
