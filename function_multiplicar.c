53bba7164b4bc53bdabbded03134203da7c1323a
Luís Guilherme <luis.bl.guilherme@gmail.com>
2026-05-27 20:39:55 -0300

    function_multiplicar.c

---
+++ function_multiplicar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//Desenvolvido por Luís Guilherme, 1° DS
+
+int multiplicar(int a, int b) 
+{
+    int resultado = 0;
+    int negativo = 0;
+
+    if (b < 0) 
+    {
+        b = -b;
+        a = -a;
+    }
+
+    for (int i = 0; i < b; i++) 
+    {
+        resultado += a;
+    }
+
+    return resultado;
+}
